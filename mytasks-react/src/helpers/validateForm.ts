a2d7f8f1b8d1f440f910f560f1ef3b69e8f1a83e
raphaelcamar <raphaelcamar@outlook.com>
2021-06-10 16:15:07 -0300

    validator for subscribe initial logic

---
+++ mytasks-react/src/helpers/validateForm.ts
@@ -0,0 +1,25 @@
+
+type formProps = {
+  name: string;
+  password: string;
+  email: string;
+  cpf: string;
+}
+
+export default function validateForm(form){
+  const {name, password, email, cpf} = form;
+
+  
+}
+
+function validateName(str){
+
+}
+
+function validateCpf(cpf){
+
+}
+
+function validateEmail(email){
+
+}
